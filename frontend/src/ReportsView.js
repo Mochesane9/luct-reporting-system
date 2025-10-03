e5dc54d07fe8a5beaf5fcc3d7dd5f1533bc1145f
Mochesane9 <mochesanelebohang9@gmail.com>
2025-10-03 20:48:15 +0200

    Update ReportsView.js

--- frontend/src/ReportsView.js
+++ frontend/src/ReportsView.js
@@ -12,7 +12,7 @@ const ReportsView = ({ user }) => {
   const fetchReports = async () => {
     try {
       const token = localStorage.getItem('token');
-      const response = await fetch('http://localhost:5000/api/reports', {
+      const response = await fetch('https://luct-reporting-system-27vd.onrender.com/api/reports', {
         headers: {
           'Authorization': `Bearer ${token}`
         }
